30af943b6be5db2988186177b4ccdf4b8a85b98d
Azeem Akhtar <akhtarazeem278@gmail.com>
2025-12-22 17:13:48 +0530

    Add variables example demonstrating basic data types

---
+++ basics/variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int age = 18;
+    float pi = 3.14;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("Pi: %.2f\n", pi);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
+
