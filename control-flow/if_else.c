4e150a00b37e78d76c075d74ec7dda4cff01ebe9
Azeem Akhtar <akhtarazeem278@gmail.com>
2025-12-22 17:42:58 +0530

    Add if-e;se control flow example in C

---
+++ control-flow/if_else.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int number = 10;
+
+    // Check if number is positive, negative, or zero
+    if (number > 0) {
+        printf("Number is positive\n");
+    } 
+    else if (number < 0) {
+        printf("Number is negative\n");
+    } 
+    else {
+        printf("Number is zero\n");
+    }
+
+    return 0;
+}
